bf1eb5372230ac0b13e83c24580b05664389e0ad
FedericoWalovnik <federico.walovnikQ>
2020-04-06 14:21:13 -0300

    Finished Float & Double Challenge

---
+++ 29_Float&DoubleChallenge/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double pounds = 1_000d;
+	    double convertedKilograms = pounds * 0.45359237;
+        System.out.println(convertedKilograms);
+
+    }
+}
